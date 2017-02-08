7c61e8f82cfac5e5248e63e540e5b424bc0c515f
周宁 <244305150@qq.com>
2017-02-08 22:40:52 +0800

    左上角框

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,2 +1,12 @@
 $(document).ready(function() {
+  $(".panel-title a").click(function() {
+    var span = $(this).find("span").first();
+    if (span.hasClass("icon-chevron-down")) {
+      span.removeClass("icon-chevron-down"); 
+      span.addClass("icon-chevron-up");
+    } else {
+      span.removeClass("icon-chevron-up"); 
+      span.addClass("icon-chevron-down");
+    }
+  });
 });
